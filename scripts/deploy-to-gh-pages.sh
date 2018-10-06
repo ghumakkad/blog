67386f09f12d55e356f3cfe9e69e215ecdd419d1
Prashant C Chaturvedi <pccfrmvns@gmail.com>
2018-10-06 17:54:20 +0530

    more changes

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
 set -o errexit
 
 rm -rf public
@@ -9,12 +8,5 @@ mkdir public
 git config --global user.email "pccfrmvns@gmail.com"
 git config --global user.name "Prashant C Chaturvedi"
 
-# build (CHANGE THIS)
-bundle exec rake --trace
-
-# deploy
-cd public
-git init
-git add .
-git commit -m "Deploy Ghumakkad Webapp to Github Pages"
-git push --force --quiet --trace "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+# build
+bundle exec rake deploy
